a1a72893f738f5ae9182137bb556837498faa330
21941 <2194150786@qq.com>
2022-08-11 01:24:06 +0800

    Changes to be committed: 	modified:   .idea/misc.xml 	modified:   app/src/main/java/com/example/dousheng/MainActivity.kt 	modified:   app/src/main/res/layout/activity_main.xml 	modified:   app/src/main/res/navigation/rank_nav.xml

--- app/src/main/java/com/example/dousheng/MainActivity.kt
+++ app/src/main/java/com/example/dousheng/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.dousheng
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+        val navHostFragment: NavHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController: NavController = navHostFragment.navController
+        val configuration: AppBarConfiguration =
+            AppBarConfiguration.Builder(bottomNavigationView.menu).build()
+        NavigationUI.setupActionBarWithNavController(this, navController, configuration)
+        NavigationUI.setupWithNavController(bottomNavigationView, navController)
     }
 }
