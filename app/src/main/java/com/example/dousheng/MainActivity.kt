7428101aa9cc1c1fd10b2ae914634b40547d2e61
siro <1220383372@qq.com>
2022-08-10 17:47:24 +0800

    添加ViewBinding

--- app/src/main/java/com/example/dousheng/MainActivity.kt
+++ app/src/main/java/com/example/dousheng/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.dousheng
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.dousheng.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            Toast.makeText(this, "Button!", Toast.LENGTH_LONG).show()
+        }
+
     }
 }
