779bf3dbf686fc8949033ecaba4605ae8ea0426f
siro <1220383372@qq.com>
2022-08-12 10:39:10 +0800

    导入SDK，配置 ViewBinding 。

--- app/src/main/java/com/qxy/dousheng/MainActivity.kt
+++ app/src/main/java/com/qxy/dousheng/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.qxy.dousheng
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.bytedance.sdk.open.douyin.DouYinOpenApiFactory
+import com.bytedance.sdk.open.douyin.DouYinOpenConfig
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val clientkey = "awr4g04kxg26jk2l" // 需要到开发者网站申请
+
+        DouYinOpenApiFactory.init(DouYinOpenConfig(clientkey))
+
     }
 }
