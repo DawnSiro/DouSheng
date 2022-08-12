599dd3c3cf6b1453730d0b66c04986f7edc85594
siro <1220383372@qq.com>
2022-08-12 22:54:03 +0800

    按 MVVM 分了层

---
+++ app/src/main/java/com/qxy/dousheng/model/BasicInformation.kt
@@ -0,0 +1,9 @@
+package com.qxy.dousheng.model
+
+data class BasicInformation(val avatar: String,
+                            val city: String,
+                            val nation: String,
+                            val nickname: String,
+                            val gender: String,
+                            val profile: String) {
+}
