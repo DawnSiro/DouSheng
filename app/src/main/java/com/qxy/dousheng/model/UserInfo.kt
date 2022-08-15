5f790221fb5185781000238a2a1e24990f1e6d10
siro <1220383372@qq.com>
2022-08-16 01:45:54 +0800

    新增申请权限

--- app/src/main/java/com/qxy/dousheng/model/UserInfo.kt
+++ app/src/main/java/com/qxy/dousheng/model/UserInfo.kt
@@ -20,3 +20,32 @@ data class UserInfo(
     val gender: String,
     @ColumnInfo(name = "profile")
     val profile: String)
+
+
+
+/*
+
+
+{
+    "data": {
+        "avatar": "https://p3.douyinpic.com/aweme/100x100/aweme-avatar/tos-cn-i-0813_c38017ebe4f14a9a9a81e98e6112bd2a.jpeg?from=4010531038",
+        "avatar_larger": "https://p3.douyinpic.com/aweme/1080x1080/aweme-avatar/tos-cn-i-0813_c38017ebe4f14a9a9a81e98e6112bd2a.jpeg?from=4010531038",
+        "captcha": "",
+        "city": "",
+        "client_key": "awr4g04kxg26jk2l",
+        "country": "",
+        "desc_url": "",
+        "description": "",
+        "district": "",
+        "e_account_role": "",
+        "error_code": 0,
+        "gender": 0,
+        "log_id": "202208151950450102080160860D0C48F7",
+        "nickname": "DawnSiro",
+        "open_id": "_000E77KMHNLpL-XFNPzk8DofgVpPAAk-e0Y",
+        "province": "",
+        "union_id": "1521d2d8-0acd-494f-9e5a-59194192b02c"
+    },
+    "message": "success"
+}
+ */
