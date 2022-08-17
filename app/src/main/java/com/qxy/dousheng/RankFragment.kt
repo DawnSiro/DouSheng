09fba6163139930b00e56657778e3aa64d833f5a
21941 <2194150786@qq.com>
2022-08-17 12:49:25 +0800

    完成页面1

--- app/src/main/java/com/qxy/dousheng/RankFragment.kt
+++ app/src/main/java/com/qxy/dousheng/RankFragment.kt
@@ -1,11 +1,16 @@
 package com.qxy.dousheng
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class RankFragment : Fragment() {
 
@@ -19,13 +24,21 @@ class RankFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_rank, container, false)
+        return inflater.inflate(R.layout.fragment_rank, container, false);
     }
 
+    @SuppressLint("ResourceType")
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this)[RankViewModel::class.java]
 
+        // load
+        val rankBottomNavigationView: BottomNavigationView =
+            requireView().findViewById(R.id.rankNavigationView)
+        val rankNavHostFragment: NavHostFragment =
+            childFragmentManager.findFragmentById(R.id.rankFragmentContainerView) as NavHostFragment
+        val navController: NavController = rankNavHostFragment.navController
+        NavigationUI.setupWithNavController(rankBottomNavigationView, navController)
     }
 
 }
--- app/src/main/java/com/qxy/dousheng/RankViewModel.kt
+++ app/src/main/java/com/qxy/dousheng/RankViewModel.kt
@@ -3,5 +3,5 @@ package com.qxy.dousheng
 import androidx.lifecycle.ViewModel
 
 class RankViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
 }
