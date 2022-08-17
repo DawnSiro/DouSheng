eaec64ad39a4e3e8b62b103b09b23f0387261064
21941 <2194150786@qq.com>
2022-08-18 00:01:47 +0800

    页面1：rename

--- app/src/main/java/com/qxy/dousheng/InfoFragment.kt
+++ app/src/main/java/com/qxy/dousheng/InfoFragment.kt
@@ -24,8 +24,8 @@ class InfoFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(InfoViewModel::class.java)
-        // TODO: Use the ViewModel
+        viewModel = ViewModelProvider(this)[InfoViewModel::class.java]
+
     }
 
 }
