4a2428893a101897d8406cfdf5738728c40c2f2b
siro <1220383372@qq.com>
2022-08-27 00:35:54 +0800

    写了一点NiceSpinner的代码

--- app/src/main/java/com/qxy/dousheng/ui/rank/ArtRankFragment.kt
+++ app/src/main/java/com/qxy/dousheng/ui/rank/ArtRankFragment.kt
@@ -1,16 +1,22 @@
 package com.qxy.dousheng.ui.rank
 
+import android.R
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.qxy.dousheng.adapter.RankAdapter
 import com.qxy.dousheng.databinding.FragmentArtRankBinding
 import com.qxy.dousheng.model.rank.RankItem
+import com.qxy.dousheng.network.OkHttpUtils
+import org.angmarch.views.NiceSpinner
+import org.angmarch.views.OnSpinnerItemSelectedListener
 
 /**
  * 综艺排行榜单 Fragment
@@ -54,8 +60,21 @@ class ArtRankFragment : Fragment() {
                 if (activity != null) {
                     adapter.rankList = it
                     adapter.notifyDataSetChanged()
+                    OkHttpUtils.getRankVersion()
                 }
             }
+
+
+            binding.spinner2.attachDataSource(OkHttpUtils.getRankVersion())
+
+            binding.spinner2.onSpinnerItemSelectedListener =
+                OnSpinnerItemSelectedListener { parent, view, position, id ->
+                    Toast.makeText(requireActivity(), "成功",Toast.LENGTH_SHORT).show() }
+
+
+
+
+
         }
     }
 }
